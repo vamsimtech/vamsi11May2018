d81a0a8b6572e4b83ab835a076adbd5634d4b513
vamsipc <vamsipc@DESKTOP-BV5IUMI>
2018-05-11 16:53:19 +0530

    first commit

---
+++ mavenArtifactID/src/test/java/mavenGroupId/mavenArtifactID/DemoforMaven.java
@@ -0,0 +1,11 @@
+package mavenGroupId.mavenArtifactID;
+
+import org.testng.annotations.Test;
+
+public class DemoforMaven {
+	@Test
+	public void testHello(){
+		System.out.println("hello this maven example");
+	}
+
+}
